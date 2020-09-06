798f61eb94195a03fadce337e72f2e84987fa786
walterdane <walterdane1@icloud.com>
2020-09-05 22:01:44 -0500

    adding base node structure

---
+++ node.py
@@ -0,0 +1,23 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+import networkparam
+
+class Node():
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent = 65537,
+            key_size=2048
+        )
+        self.public_key = self.private_key.public_key()
+    
+    def sign_message(self, message):
+        signature = self.private_key.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256(),
+        )
